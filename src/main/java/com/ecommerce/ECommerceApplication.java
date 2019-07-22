e97b5bcfed3d49cce8cc52a592d79be7025ede69
Surya Kant Bansal <suryakantbansal97@gmail.com>
2019-07-22 21:58:22 +0530

    setup packages

--- src/main/java/com/ecommerce/ECommerceApplication.java
+++ src/main/java/com/ecommerce/ECommerceApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("com.example.demo.model.persistence.repositories")
-@EntityScan("com.example.demo.model.persistence")
+@EnableJpaRepositories("com.ecommerce.model.persistence.repositories")
+@EntityScan("com.ecommerce.model.persistence")
 @SpringBootApplication
 public class ECommerceApplication {
 
